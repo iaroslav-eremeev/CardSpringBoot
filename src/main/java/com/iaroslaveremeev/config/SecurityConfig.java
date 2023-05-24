e887266903062b4db8bf1fa952bff3c06e96c718
Iaroslav <eremeev.pt@gmail.com>
2023-05-24 16:50:51 +0500

    Add security configuration to allow JS files to work

--- src/main/java/com/iaroslaveremeev/config/SecurityConfig.java
+++ src/main/java/com/iaroslaveremeev/config/SecurityConfig.java
@@ -2,20 +2,26 @@ package com.iaroslaveremeev.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
-
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .antMatchers("/templates/login.html").permitAll()
+                .antMatchers("/templates/login.html", "/templates/registration.html", "/static/scripts/**").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .formLogin()
                 .loginPage("/templates/login.html")
                 .permitAll();
+    }
+
+    public void configure(WebSecurity web) throws Exception {
+        web
+                .ignoring()
+                .antMatchers("/scripts/**");
     }
 }
